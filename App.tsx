2414638d30df153a2e9f5a0ee5b15569c5a3286b
Pooja <poojab9505@gmail.com>
2025-04-19 16:56:07 -0500

    Updated mockData, app configured with Redux

--- App.tsx
+++ App.tsx
@@ -1,22 +1,26 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import { StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { RootStackParamList } from './src/types/navigation.types';
 import EmployeeListing from './src/screens/EmployeeListing';
 import EmployeeDetail from './src/screens/EmployeeDetail';
+import { store } from './src/store';
 
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="EmployeeListing">
-        <Stack.Screen name="EmployeeListing" component={EmployeeListing} />
-        <Stack.Screen name="EmployeeDetail" component={EmployeeDetail} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Provider store={store}>
+      <NavigationContainer>
+        <Stack.Navigator id={undefined} initialRouteName="EmployeeListing">
+          <Stack.Screen name="EmployeeListing" component={EmployeeListing} />
+          <Stack.Screen name="EmployeeDetail" component={EmployeeDetail} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </Provider>
   );
 }
 
